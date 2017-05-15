e04ef04a71e2a6e0345226da1e7858ebe08d96f5
Minghao Li <ml653@cornell.edu>
2017-05-15 09:45:52 -0700

    bump

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  validates :username, :email, :password_digest, :img_url, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { within: 6...64, allow_blank: true }
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.password_is?(password) ? user : nil
+  end
+
+  def password_is?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(32)
+  end
+
+private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+end
